d1b257cdccb67dcd76854b0e86f7a05b219f2986
Willamy Braga <52117695+willamyBraga@users.noreply.github.com>
2020-06-25 20:12:29 -0300

    Add files via upload

---
+++ src/component/Display.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="display">
+      {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['value']
+}
+</script>
+
+<style>
+    .display{
+        grid-column:  span 4;
+        background-color: #0004;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 20px;
+        font-size: 2em;
+        overflow: hidden;
+    }
+</style>
